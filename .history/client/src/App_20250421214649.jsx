8afb9b66ec2c8bda14ceddd084008527be4e0f38
priyab2496 <bansal04.p@gmail.com>
2025-04-21 22:09:42 +0530

    first commit

---
+++ .history/client/src/App_20250421214649.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataList from './components/DataList';
+
+function App() {
+  return (
+    <div className="App">
+      <DataList />
+    </div>
+  );
+}
+
+export default App;
